4ae1a1e6831c7b82c67684bb4f82eb6551869f49
Reinert <lucas_silva187@estudante.sesisenai.org.br>
2026-07-21 15:53:37 -0300

    feat: cria estrutura inicial de pedidos e carrinho

--- src/modelos/Produto.java
+++ src/modelos/Produto.java
@@ -1,7 +1,7 @@
 package modelos;
 
 public class Produto {
-    private int id;
+    private Integer id;
     private String nome;
     private double preco;
     private int estoque;
@@ -19,11 +19,11 @@ public class Produto {
     }
 
     // Getters e Setters
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
